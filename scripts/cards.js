d0c6806835cb59fd962d17beaa344ede65c511ee
Timophey <ltimophey06@yandex.ru>
2024-11-18 01:30:22 +0300

    fix: cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "село Довольное",
+      link: "https://novosibirsk.spravedlivo.ru/depot/pict/030/03048206000398.jpg",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Манеж им. Братьев Знаменских",
+      link: "https://www.trackandfield.ru/images/uploads/news/8217.4837-b.gif",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Колодки",
+      link: "https://sports-tech.ru/upload/iblock/3e9/startovye-kolodki-universalnye-polanik-pbs-01-otsinkovannaya-stal.jpg",
     },
     {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+      name: "Тренболон",
+      link: "https://sun1-56.userapi.com/impg/QoJtHDrp8qPs3Gtr6PjuID9LCJ0S-tRd3fxhoQ/4kplq-T1Q5Y.jpg?size=768x772&quality=95&sign=4d6c01f5c36068e9dcbbb7bd2b411a5f&c_uniq_tag=AuyJ8BYfzL-HEFloTT69I7VH2RmfF8c1CrBDSY40tC8&type=album",
     },
     {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+      name: "Даня Штапкин",
+      link: "https://sun9-20.userapi.com/impg/7zQSspd65gbSm1QjULCQkBCChfBqNnX06DvYPw/XhDFfRZc3MU.jpg?size=1080x1352&quality=95&sign=92deb8928fd69a5cb27ba8e147c619a0&type=album",
     },
     {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+      name: "Штанга 100кг",
+      link: "https://klokovequipment.ru/wp-content/uploads/2018/11/shtanga-klokovd-1.png",
+    },
 ];
